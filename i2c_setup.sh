9ff3a7ec07a2caf1e07ac08388467446c627375b
samAlti <sam@altitude.pw>
2017-02-16 17:06:51 +0000

    removed apt-get dist-upgrade
    removed apt-get dist-upgrade to stop the install breaking some peoples kernels

--- i2c_setup.sh
+++ i2c_setup.sh
@@ -3,7 +3,6 @@
 
 apt-get update
 apt-get upgrade
-apt-get dist-upgrade
 
 apt-get install i2c-tools python-smbus python3-smbus python-dev python3-dev -f
 echo "dtparam=i2c_arm=on" >> /boot/config.txt
